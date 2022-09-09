a6da28eafd4ad77d3a1fe33463941440547a60c0
Chelsea Hua <92641253+chels86183@users.noreply.github.com>
2022-09-09 13:13:58 -0400

    Add files via upload

---
+++ Helloworld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+//First C++ script 
+int main()
+{
+    std::cout << "Hello World " << std::endl;
+}
+
+// using namespace std;
+
+// int test() {
+//     for (int i = 1; i <= 5; ++i) {
+//         cout <<  "Hello World! " << endl;
+//     }
+//     return 0;
+// }
